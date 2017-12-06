dec536bd2e821382e141a32355abc2ff85622d80
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-06 13:59:45 -0500

    Update howManyWords.py

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,15 @@
+#Claire Yegian
+#12/6/17
+#howManyWords.py - prints number of words for each number of letters
 
+dictionary = open('engmix.txt')
+
+letters = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+i = 1
+for word in dictionary:
+    if len(word) = i:
+        letters[i-1] = (letters[i-1] + 1)
+
+for i in (0,9):
+    
