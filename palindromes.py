de5b0cd907a14a9b18dc7f44253cd5afc81de118
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-07 14:37:17 -0500

    Updated from Brython Server: 12/7/2017 2:37:44 PM

--- palindromes.py
+++ palindromes.py
@@ -5,9 +5,11 @@
 dictionary = open('engmix.txt')
 
 for word in dictionary:
+    word = word.strip()
     wordList = list(word)
+    wordList.reverse()
     wordReverse = ''
     for ch in wordList:
-        wordReverse = wordReverse + ch
-    if word == wordList.reverse:
-        print('yay')
+        wordReverse = (wordReverse+ch)
+    if word == wordReverse:
+        print(word)
