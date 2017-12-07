643b4791c1fa36f1c218e5dafbef99949143832d
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-07 14:18:33 -0500

    Updated from Brython Server: 12/7/2017 2:19:00 PM

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,8 @@
+#Claire Yegian
+#12/7/17
+#reverseFile.py - asks for name of file and prints all lines of file in reverse
 
+dictionary = open('engmix.txt')
+
+dictionary.reverse()
+print(dictionary)
