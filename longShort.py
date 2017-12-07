5fd0d4e3e6d5d7436a6d9b9df099355722cce163
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-07 14:55:04 -0500

    Updated from Brython Server: 12/7/2017 2:55:31 PM

--- longShort.py
+++ longShort.py
@@ -1,1 +1,9 @@
+#Claire Yegian
+#12/7/17
+#longShort.py - prints shortest and longest words for each letter of alphabet
 
+dictionary = open('engmix.txt')
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+longest =['']*26
