d6cfe2da0e1b8b55e590bbf2e8f778ab526bbee7
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-07 14:29:20 -0500

    Updated from Brython Server: 12/7/2017 2:29:47 PM

--- palindromes.py
+++ palindromes.py
@@ -1,1 +1,13 @@
+#Claire Yegian
+#12/7/17
+#palindromes.py - prints all palindromes in dictionary
 
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    wordList = list(word)
+    wordReverse = ''
+    for ch in wordList:
+        wordReverse = wordReverse + ch
+    if word == wordList.reverse:
+        print('yay')
