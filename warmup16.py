6eea4cfcde645530da2ebb3d4a03f074679d5782
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-11 14:18:35 -0500

    Updated from Brython Server: 12/11/2017 2:19:18 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,10 @@
+#Claire Yegian
+#12/11/17
+#warmup16.py - all words that start with 'c' and end with 'y'
 
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    if word.strip()[0] == 'c':
+        if word.strip()[-1] == 'y':
+            print(word)
