a593ab6a0d7139585e444257a17924a71e4efe65
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-11 14:28:51 -0500

    Updated from Brython Server: 12/11/2017 2:29:34 PM

--- longShort.py
+++ longShort.py
@@ -7,3 +7,23 @@ dictionary = open('engmix.txt')
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 longest =['']*26
+shortest = ['']*26
+alphabetNum = 0
+for word in dictionary:
+    word = word.strip()
+    if word != '':
+        letter = alphabet[alphabetNum]
+        letterList = []
+        while word[0] == letter:
+            letterList.append(word)
+            longestWord.letter = 0
+            longestword.letter = ''
+            for line in letterList:
+                if len(line)>=longestWord.letter:
+                    longestWord.letter = len(line)
+                    longestword.letter = word
+            longest[alphabetNum] = longestword.letter
+            shortestWord.letter = longestWord.letter
+            shortestword.letter = ''
+            for line in letterList:
+                if len(line)<=
