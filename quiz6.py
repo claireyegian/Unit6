dea5641d475e14b2cb493088b4a18f9751729e5b
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-13 13:33:55 -0500

    Updated from Brython Server: 12/13/2017 1:34:39 PM

--- quiz6.py
+++ quiz6.py
@@ -5,17 +5,12 @@
 dictionary = open('engmix.txt')
 
 #Program 1
-cList = []
-pList = []
+list = []
 for word in dictionary:
-    word.split()
-    if word.count('c') == 3:
-        cList.append(word)
-    if word.count('p') == 2:
-        pList.append(word)
-for word in cList:
-    print(word+'has 3 cs')
-for word in pList:
-    print(word+'has 2 ps')
+    if word.count('c') == 3 and word.count('p') == 2:
+        list.append(word)
+for word in list:
+    word.strip()
+    print(word)
     
 
